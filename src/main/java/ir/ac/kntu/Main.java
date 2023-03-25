3d39c140eb9fd89da899387505a21074e6bffc58
Mohamadhosein Shabaniraad <mh.shabanirad@icloud.com>
2023-03-25 03:47:19 +0330

    add checkPackage

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -3,6 +3,7 @@ package ir.ac.kntu;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Main {
@@ -11,20 +12,38 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         String fileName = scanner.nextLine();
         BufferedReader reader;
+        ArrayList<String> file = new ArrayList<>();
         try {
             reader = new BufferedReader(new FileReader("src/main/java/ir/ac/kntu/" + fileName));
             String line = reader.readLine();
 
             while (line != null) {
                 System.out.println(line);
-                // read next line
-                //dskhskhd
+                file.add(line);
                 line = reader.readLine();
             }
+            checkStyle(file);
             reader.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+    }
+
+    public static void checkStyle(ArrayList<String> input) {
+        checkPackage(input);
+
+    }
+
+    public static int checkPackage(ArrayList<String> input) {
+        for (String s : input) {
+            if (s.contains("package")) {
+                if (input.indexOf(s) != 0) {
+                        System.out.println("Error in use package in line" + (input.indexOf(s)+1)  + " must write in line 1");
+
+                }
+            }
+        }
+        return 0;
     }
 
 }
--- src/main/java/ir/ac/kntu/Test.java
+++ src/main/java/ir/ac/kntu/Test.java
@@ -1,3 +1,4 @@
+
 package ir.ac.kntu;
 
 public class Test {
