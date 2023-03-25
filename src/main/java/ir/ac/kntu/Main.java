0ab7ca96ca2a094c64a4adc332d8861cf6cca3c6
Mohamadhosein Shabaniraad <mh.shabanirad@icloud.com>
2023-03-26 03:20:23 +0330

    check all line for func

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -38,44 +38,56 @@ public class Main {
     }
 
     public static int checkPackage(ArrayList<String> input) {
-
+        boolean findeError = false;
         for (String s : input) {
             if (s.contains("package")) {
                 if (input.indexOf(s) != 0) {
                     int line = input.indexOf(s);
                     System.out.printf("Warring Line %d in use package | must write in line 1\n", line);
-                    return 1;
+                    findeError = true;
                 }
             }
         }
         if (input.get(0).length() != 0) {
             if (!input.get(0).matches("package [A-Za-z.]+;") && !input.get(0).startsWith("import")) {
                 System.out.println("Warring Line 1 in use package\n");
+                findeError = true;
             }
+        }
+        if (findeError) {
+            return 1;
         }
         return 0;
     }
 
     public static int checkImports(ArrayList<String> input) {
+        boolean findError = false;
         for (String s : input) {
             if (s.startsWith("import")) {
                 if (!s.matches("import [A-Za-z.*]+;")) {
                     int line = input.indexOf(s);
                     System.out.printf("Warring Line %d in use import\n", line + 1);
-                    return 1;
+                    findError = true;
                 }
             }
+        }
+        if (findError) {
+            return 1;
         }
         return 0;
     }
 
     public static int check80Char(ArrayList<String> input) {
+        boolean findError = false;
         for (String s : input) {
             if (s.length() > 80) {
                 int line = input.indexOf(s);
                 System.out.printf("Warring Line %d longer than 80 charachter\n", line + 1);
-                return 1;
+                findError = true;
             }
+        }
+        if (findError) {
+            return 1;
         }
         return 0;
     }
