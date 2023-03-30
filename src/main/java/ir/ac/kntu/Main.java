8dd222823fc5ab4ba1d63792ad316ad71c8d7251
Mohamadhosein Shabaniraad <mh.shabanirad@icloud.com>
2023-03-30 06:40:04 +0330

    rename file

--- src/main/java/ir/ac/kntu/Main.java
+++ src/main/java/ir/ac/kntu/Main.java
@@ -6,7 +6,7 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class main {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
